30fe95d7766e89c821084eba9233e30d36b71a0f
LeinneSW <nno88551@naver.com>
2024-11-09 22:27:05 +0900

    param 여부에 따라 화면이 달라지도록 변경 (param有: OBS용, 無: 브라우저용)

--- public/chat.js
+++ public/chat.js
@@ -1,13 +1,24 @@
 import {ChzzkClient} from "https://cdn.skypack.dev/chzzk"
 
-window.onload = async () => {
-    const params = new URLSearchParams(location.search);
-    const channelId = params.get('channel');
-    if(!channelId){
-        alert('채널 아이디를 올바르게 작성해주세요');
+let chzzkChat;
+let lastTryMillis = 0;
+
+async function connectChannel(channelId){
+    if(Date.now() - lastTryMillis < 2000){ // 과한 요청 방지
         return;
     }
 
+    if(!channelId){
+        return false;
+    }else if(channelId.length !== 32){
+        alert('채널 아이디를 올바르게 작성해주세요');
+        return false;
+    }
+
+    lastTryMillis = Date.now();
+    if(chzzkChat?.connected){
+        chzzkChat.disconnect();
+    }
     const client = new ChzzkClient({
         baseUrls: {
             chzzkBaseUrl: "/cors/chzzk",
@@ -19,15 +30,15 @@ window.onload = async () => {
         liveDetail = await client.live.detail(channelId);
     }catch(e){
         alert(e.message);
-        return;
+        return false;
     }
 
     if(!liveDetail){
         alert('채널 아이디를 올바르게 작성해주세요');
-        return;
+        return false;
     }
 
-    const chzzkChat = client.chat({
+    chzzkChat = client.chat({
         channelId: liveDetail.channel.channelId,
         pollInterval: 30 * 1000
     });
@@ -38,5 +49,21 @@ window.onload = async () => {
         }
         addTTSQueue(data.message);
     });
-    chzzkChat.connect();
+    try{
+        await chzzkChat.connect();
+        addTTSQueue('TTS가 활성화 되었습니다.');
+        return true;
+    }catch{}
+    return false;
+}
+
+window.onload = async () => {
+    const params = new URLSearchParams(location.search);
+    const success = await connectChannel(params.get('channel') || params.get('channelId'));
+    if(!success){
+        document.body.innerHTML = '채널ID: <input type="text" id="text"><br><input type="button" id="button" value="접속">';
+        button.onclick = () => connectChannel(text.value);
+    }else{
+        document.body.style.background = 'background: rgba(0, 0, 0, 0)';
+    }
 };
