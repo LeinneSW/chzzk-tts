e11341450ce9e03db9006633dc8b3073edf1680c
LeinneSW <nno88551@naver.com>
2024-11-11 13:29:58 +0900

    반복 글자 파악 로직관련 버그 수정, 이모지의 경우도 최대 3회만 반복하도록 개선

--- public/tts.js
+++ public/tts.js
@@ -1,11 +1,13 @@
 const ttsQueue = [];
 let isPlaying = false;
 
+const emojiRegex = /\p{Emoji}/u; // 이모지 구분 정규식
+
 const addTTSQueue = (text) => {
     let repeatData = null;
 
     // 반복 글자 파악 로직(도배 방지를 위해 추가)
-    const len = Math.ceil(text.length / 4);
+    const len = Math.floor(text.length / 4);
     for(let i = 1; i <= len; ++i){ // 문자열의 길이의 1/4 까지만 확인(4회이상 반복하는지 파악)
         let index = 0, count = 1;
         const substring = text.substring(0, i);
@@ -14,11 +16,13 @@ const addTTSQueue = (text) => {
         }
         if(count > 3 && count * substring.length === text.length){
             repeatData = {substring, count};
+            break;
         }
     }
     if(repeatData){
         const {substring, count} = repeatData;
-        text = substring.repeat(substring.length < 3 ? Math.min(count, 8) : 3);
+        const isEmoji = emojiRegex.test(substring);
+        text = substring.repeat(isEmoji || substring.length >= 3 ? 3 : Math.min(count, 8));
     }
     ttsQueue.push(text);
     processQueue();
